5d004381218548271c970dbf55ced6048acb04be
Mukta <muktadanu1995@gmail.com>
2021-01-31 19:14:40 +0530

    three commit

---
+++ src/test/java/integrationDemo/integration1/ThirdTest.java
@@ -0,0 +1,15 @@
+package integrationDemo.integration1;
+
+import org.testng.annotations.Test;
+
+public class ThirdTest {
+
+	@Test
+	public void methodthreetest()
+	{
+		for(int i = 1; i <=10; i++)
+		{
+			System.out.println("Loop : " + i );
+		}
+	}
+}
