9f925b808fed5720bc1f0d626805b6a39be401ee
Mukta <muktadanu1995@gmail.com>
2021-01-31 16:53:43 +0530

    Second commit

---
+++ src/test/java/integrationDemo/integration1/SecondTest.java
@@ -0,0 +1,12 @@
+package integrationDemo.integration1;
+
+import org.testng.annotations.Test;
+
+public class SecondTest {
+
+	@Test
+	public void methodtwotest()
+	{
+		System.out.println("Second Test....");
+	}
+}
