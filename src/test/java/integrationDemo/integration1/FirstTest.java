8eb2237545be7d4e5a4ec596980f130df02cb3ef
Mukta <muktadanu1995@gmail.com>
2021-01-31 16:43:04 +0530

    First commit

---
+++ src/test/java/integrationDemo/integration1/FirstTest.java
@@ -0,0 +1,12 @@
+package integrationDemo.integration1;
+
+import org.testng.annotations.Test;
+
+public class FirstTest {
+
+	@Test
+	public void methodOneTest()
+	{
+		System.out.println("First method...");
+	}
+}
